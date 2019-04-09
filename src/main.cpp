85c48d7a36d6268e433f929af634c5491ce5f1d4
JohnBardoe <yta1@list.ru>
2019-04-09 09:04:57 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+void turnAng(int ang){}
+void setSpeed(int speed){}
+
+struct Pair{
+  int x, y;
+
+};
+
+int map[4][8];
+
+int adjList[4][8][4];
+
+void setup() {
+
+}
+
+void loop() {
+
+}
