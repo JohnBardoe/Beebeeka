14b947ce7fc1d32fd6b59e2d9dd0ee60c0b42027
Egor Sokolov <egor1sokolov@gmail.com>
2019-04-13 19:31:20 +0300

    test

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -1,3 +1,16 @@
-class BFs{
-    private: int x_st, y_st, x_f, y_f;
+#include <vector.h>
+class Bfs{
+    public:
+        int x_st, y_st, x_f, y_f;
+        int map_f [4][8][4];
+    void change()
+    {
+        
+    }
+    
+    void bfs()
+    {
+        
+    }
+
 };
